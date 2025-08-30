aec33ff98f0f7afe3360d54b98c2327c55a18a98
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-08-30 12:28:52 +0530

    Create circumference and area of circle.c

---
+++ circumference and area of circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    float radius, area, circumference, PI= 3.14;
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+    printf("Area of the circle = %f\n", area);
+    printf("Circumference of the circle = %f\n", circumference);
+
+    return 0;
+}
