c1ed6fa0cea13f7cef922efe982dec0d48a88c2d
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-08-30 12:07:40 +0530

    day1.c

---
+++ day1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int a,b;
+  printf("enter the number");
+  scanf("%d %d",&a ,&b);
+  int c=a+b;
+  printf("sum of two elements is%d\n",c);
+    return 0;
+}//sum of two number
