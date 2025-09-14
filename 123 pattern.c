1e6f8b0d8c07b846f02dca8499468d8b9cb4ca9c
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-09-14 23:06:34 +0530

    Create 123 pattern.c

---
+++ 123 pattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {        // rows
+        for (j = 1; j <= i; j++) {    // numbers in each row
+            printf("%d", j);
+        }
+        printf("\n");  // move to next line
+    }
+
+    return 0;
+}
