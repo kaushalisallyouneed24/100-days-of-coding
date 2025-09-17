59129130e1847be29bf02ee763206cc9aaca025f
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-09-17 23:03:33 +0530

    Create &isprime.c

---
+++ &isprime.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers from 1 to %d are:\n", n);
+
+    for(i = 2; i <= n; i++) {     // start from 2
+        isPrime = 1;              // assume i is prime
+        for(j = 2; j <= i/2; j++) {
+            if(i % j == 0) {
+                isPrime = 0;      // not prime
+                break;
+            }
+        }
+        if(isPrime == 1) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
