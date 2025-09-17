89491957ddf79e2753b44b87a254989f4bb7fde9
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-09-17 22:54:48 +0530

    Create numeric pattern.c

---
+++ numeric pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 1; i <= 5; i++) {         // rows
+        for(j = 1; j <= i; j++) {     // print numbers from 1 to i
+            printf("%d", j);
+        }
+        printf("\n");  // move to next line
+    }
+
+    return 0;
+}
