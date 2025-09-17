5880066ec43a559ffc582f7e42e6640645f1f80a
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-09-17 23:02:11 +0530

    Create read and prnt array.c

---
+++ read and prnt array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];  // declare array
+
+    // Read elements
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Print elements
+    printf("Array elements are:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
