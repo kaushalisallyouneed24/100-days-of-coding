166e9ff98c2fffa30fe5189d8d3b785fb273205a
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-09-25 09:08:12 +0530

    Create max min in array .c

---
+++ max min in array .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,max,min;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+    }
+    max=min=a[0];
+    for(i=1;i<n;i++) {
+        if(a[i]>max) max=a[i];
+        if(a[i]<min) min=a[i];
+    }
+    printf("%d %d",max,min);
+    return 0;
+}
