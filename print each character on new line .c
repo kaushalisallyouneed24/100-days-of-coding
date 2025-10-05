10ed628a5f1ba7163d0ba8809c1b107d5b968082
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-10-05 10:21:09 +0530

    Create print each character on new line .c

---
+++ print each character on new line .c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); // Read string including spaces
+
+    // Print each character on a new line
+    while(str[i] != '\0') {
+        // Skip newline character from fgets
+        if(str[i] != '\n') {
+            printf("%c\n", str[i]);
+        }
+        i++;
+    }
+
+    return 0;
+}
