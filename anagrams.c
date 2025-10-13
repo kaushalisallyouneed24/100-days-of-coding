5c60facc470809faef9dec7b6c792435cd58ba0b
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-10-13 14:43:40 +0530

    Update anagrams.c

--- anagrams.c
+++ anagrams.c
@@ -6,25 +6,25 @@ int main() {
     int count1[256] = {0}, count2[256] = {0};
     int i;
 
-    // Input two strings
+
     printf("Enter first string: ");
     gets(str1);
     printf("Enter second string: ");
     gets(str2);
 
-    // If lengths differ, they cannot be anagrams
+    
     if (strlen(str1) != strlen(str2)) {
         printf("Not Anagrams\n");
         return 0;
     }
 
-    // Count frequency of each character
+    
     for (i = 0; str1[i] != '\0'; i++) {
         count1[(unsigned char)str1[i]]++;
         count2[(unsigned char)str2[i]]++;
     }
 
-    // Compare character frequencies
+    
     for (i = 0; i < 256; i++) {
         if (count1[i] != count2[i]) {
             printf("Not Anagrams\n");
